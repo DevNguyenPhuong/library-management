eb96462a61e4be5a06c6a0f3547e854471fea726
DevNguyenPhuong <dev.nguyenphuong@gmail.com>
2024-10-16 07:17:05 +0700

    add searching and pagination for PATRON

--- src/components/Patrons/PatronsTable.jsx
+++ src/components/Patrons/PatronsTable.jsx
@@ -1,52 +1,73 @@
-import { Table } from "antd";
-import { useRef, useState } from "react";
-import { FakePatronData } from "../../data/fakeData";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { Button, Space, Tooltip } from "antd";
+import { getAllData } from "../../services/apiLibrary";
 import { PATRON_PAGE_SIZE } from "../../utils/constants";
-import { getPatronColumns } from "./patronColumns";
-import { prepareTableData } from "../Table/tableUtils";
-import { useNavigate } from "react-router-dom";
+import { ReusableDataTable } from "../UI/Table/ReuseableDataTable";
 
-function PatronsTable() {
-  const searchInput = useRef(null);
-  const [searchText, setSearchText] = useState("");
-  const [searchedColumn, setSearchedColumn] = useState("");
-  const navigate = useNavigate();
-
-  const handleEdit = (id) => {
-    navigate(`/patrons/${id}`);
-  };
+export default function BooksTable() {
+  const columns = [
+    { header: "Name", accessor: "name" },
+    { header: "Phone", accessor: "phone" },
+    { header: "Gender", accessor: "gender" },
+    { header: "Status", accessor: "Status" },
+    { header: "Currently borrowed", accessor: "currentlyBorrowed" },
+    { header: "Membership Date", accessor: "membershipDate" },
+    { header: "Actions", accessor: "actions" },
+  ];
 
   const handleDetail = (id) => {
-    console.log(`Deleting patron with id: ${id} `);
+    console.log(`Patron ID: ${id}`);
+    // You can add more complex logic here, such as opening a modal or navigating to a detail page
   };
 
-  const columns = getPatronColumns(
-    searchInput,
-    searchText,
-    setSearchText,
-    searchedColumn,
-    setSearchedColumn,
-    handleEdit,
-    handleDetail
-  );
+  const handleDelete = (id) => {
+    console.log(`Patron ID: ${id}`);
+  };
 
-  const emptyRowsCount = Math.max(
-    0,
-    PATRON_PAGE_SIZE - (FakePatronData.length % PATRON_PAGE_SIZE)
+  const renderRow = (patron, columns) => (
+    <tr key={patron.id}>
+      {columns.map((column) => (
+        <td
+          key={column.accessor}
+          className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
+        >
+          {column.accessor === "actions" && (
+            <Space size="middle">
+              <Tooltip title="Edit">
+                <Button
+                  type="primary"
+                  icon={<EditOutlined />}
+                  onClick={() => handleDetail(patron.id)}
+                />
+              </Tooltip>
+              <Tooltip title="Delete">
+                <Button
+                  type="primary"
+                  danger
+                  icon={<DeleteOutlined />}
+                  onClick={() => handleDelete(patron.id)}
+                />
+              </Tooltip>
+            </Space>
+          )}
+          {!["actions"].includes(column.accessor) && patron[column.accessor]}
+        </td>
+      ))}
+    </tr>
   );
-
-  const dataWithEmptyRows = prepareTableData(FakePatronData, emptyRowsCount);
 
   return (
-    <Table
+    <ReusableDataTable
+      queryFn={({ page, size, query }) =>
+        getAllData(
+          `/patrons?page=${page}&size=${PATRON_PAGE_SIZE}&query=${query}`
+        )
+      }
+      searchPlaceHolder={"Search by name or id..."}
+      queryKey={["patrons"]}
       columns={columns}
-      dataSource={dataWithEmptyRows}
-      pagination={{
-        pageSize: PATRON_PAGE_SIZE,
-        showQuickJumper: true,
-      }}
+      renderRow={renderRow}
+      pageSize={PATRON_PAGE_SIZE}
     />
   );
 }
-
-export default PatronsTable;
